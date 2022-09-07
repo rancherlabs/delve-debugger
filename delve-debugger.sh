9ddb012f254bc8712d32d8557763e74c28e77619
Silvio Moioli <silvio@moioli.net>
2022-09-07 10:39:52 +0200

    Slight script improvements
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- delve-debugger.sh
+++ delve-debugger.sh
@@ -6,5 +6,8 @@ POD=$2
 CONTAINER=$3
 EXECUTABLE=$4
 
-kubectl --namespace $1 debug pod/$2 --image=ghcr.io/moio/delve-debugger:latest --target=$3 --env="[EXECUTABLE=$4]"
-kubectl --namespace $1 port-forward pod/$2 4000:4000
+kubectl --namespace $1 port-forward pod/$2 4000 &
+echo `tput bold`
+echo 'Please wait for the line "debug layer=debugger continuing" to appear...'
+echo `tput sgr0`
+kubectl --namespace $1 debug -it pod/$2 --image=ghcr.io/moio/delve-debugger:latest --target=$3 --env="[EXECUTABLE=$4]"
