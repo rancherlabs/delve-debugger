2d20d08c73dd4d4f29d876fc1c67ae5202143ec4
Silvio Moioli <silvio@moioli.net>
2022-09-09 10:45:10 +0200

    Pull specific version, not latest
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- delve-debugger.sh
+++ delve-debugger.sh
@@ -10,4 +10,4 @@ kubectl --namespace $1 port-forward pod/$2 4000 &
 echo `tput bold`
 echo 'Please wait for the line "debug layer=debugger continuing" to appear...'
 echo `tput sgr0`
-kubectl --namespace $1 debug -it pod/$2 --image=ghcr.io/moio/delve-debugger:latest --target=$3 --env="[EXECUTABLE=$4]"
+kubectl --namespace $1 debug -it pod/$2 --image=ghcr.io/moio/delve-debugger:1.9.1-1 --target=$3 --env="[EXECUTABLE=$4]"
