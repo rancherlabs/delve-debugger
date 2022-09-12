cbbaca5f46c2055aebbf999e43c9824b792c5cf5
Silvio Moioli <silvio@moioli.net>
2022-09-12 12:02:02 +0200

    Kill port forwarding on exit
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- delve-debugger.sh
+++ delve-debugger.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 set -e
 
+source versions
 NAMESPACE=$1
 POD=$2
 CONTAINER=$3
 EXECUTABLE=$4
 
 kubectl --namespace $1 port-forward pod/$2 4000 &
+PORT_FORWARDING_PID=$!
 echo `tput bold`
 echo 'Please wait for the line "debug layer=debugger continuing" to appear...'
 echo `tput sgr0`
 kubectl --namespace $1 debug -it pod/$2 --image=ghcr.io/moio/delve-debugger:1.9.1-1 --target=$3 --env="[EXECUTABLE=$4]"
+kill ${PORT_FORWARDING_PID}
