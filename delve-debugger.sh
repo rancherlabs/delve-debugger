e4d1de696671fe3c740afa9462a327301f93fc89
Silvio Moioli <silvio@moioli.net>
2022-09-16 11:20:35 +0200

    Bugfix: source file correctly
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- delve-debugger.sh
+++ delve-debugger.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-source versions
+source ./versions
 NAMESPACE=$1
 POD=$2
 CONTAINER=$3
@@ -12,5 +12,5 @@ PORT_FORWARDING_PID=$!
 echo `tput bold`
 echo 'Please wait for the line "debug layer=debugger continuing" to appear...'
 echo `tput sgr0`
-kubectl --namespace $1 debug -it pod/$2 --image=ghcr.io/moio/delve-debugger:$DLV_VERSION-DELVE_DEBUGGER_VERSION --target=$3 --env="[EXECUTABLE=$4]"
+kubectl --namespace $1 debug -it pod/$2 --image=ghcr.io/moio/delve-debugger:$DLV_VERSION-$DELVE_DEBUGGER_VERSION --target=$3 --env="[EXECUTABLE=$4]"
 kill ${PORT_FORWARDING_PID}
