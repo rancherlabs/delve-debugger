bd3fbc4a0f0d9a177c033c9f025d6b1e0c9bbf48
Silvio Moioli <silvio@moioli.net>
2022-09-16 11:19:14 +0200

    Use ./version in scripts
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- delve-debugger-docker.sh
+++ delve-debugger-docker.sh
@@ -8,4 +8,4 @@ EXECUTABLE=$2
 # --privileged note: this is needed in order to debug --privileged containers
 # in other cases, --cap-add=SYS_ADMIN --cap-add=SYS_PTRACE may suffice instead
 
-docker run --name delve-debugger --privileged --pid=container:$CONTAINER --env EXECUTABLE=$EXECUTABLE --publish 127.0.0.1:4000:4000/tcp --rm ghcr.io/moio/delve-debugger:1.9.1-1
+docker run --name delve-debugger --privileged --pid=container:$CONTAINER --env EXECUTABLE=$EXECUTABLE --publish 127.0.0.1:4000:4000/tcp --rm ghcr.io/moio/delve-debugger:$DLV_VERSION-DELVE_DEBUGGER_VERSION
--- delve-debugger.sh
+++ delve-debugger.sh
@@ -12,5 +12,5 @@ PORT_FORWARDING_PID=$!
 echo `tput bold`
 echo 'Please wait for the line "debug layer=debugger continuing" to appear...'
 echo `tput sgr0`
-kubectl --namespace $1 debug -it pod/$2 --image=ghcr.io/moio/delve-debugger:1.9.1-1 --target=$3 --env="[EXECUTABLE=$4]"
+kubectl --namespace $1 debug -it pod/$2 --image=ghcr.io/moio/delve-debugger:$DLV_VERSION-DELVE_DEBUGGER_VERSION --target=$3 --env="[EXECUTABLE=$4]"
 kill ${PORT_FORWARDING_PID}
