9e108cb1fa57d4f44ed470071d853cbd7efd3e62
Silvio Moioli <silvio@moioli.net>
2026-03-26 12:22:32 +0100

    delve-debugger-docker: fix after versions file was abandoned

--- delve-debugger-docker.sh
+++ delve-debugger-docker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-source ./versions
 CONTAINER=$1
 EXECUTABLE=$2
 LOCAL_PORT="${5:-4000}"
+VERSION="${VERSION:-latest}"
 
 # --privileged note: this is needed in order to debug --privileged containers
 # in other cases, --cap-add=SYS_ADMIN --cap-add=SYS_PTRACE may suffice instead
 
-docker run --name delve-debugger --privileged --pid=container:$CONTAINER --env EXECUTABLE=$EXECUTABLE --publish 127.0.0.1:$LOCAL_PORT:4000/tcp --rm ghcr.io/rancherlabs/delve-debugger:$DLV_VERSION-$DELVE_DEBUGGER_VERSION
+docker run --name delve-debugger --privileged --pid=container:$CONTAINER --env EXECUTABLE=$EXECUTABLE --publish 127.0.0.1:$LOCAL_PORT:4000/tcp --rm ghcr.io/rancherlabs/delve-debugger:$VERSION
